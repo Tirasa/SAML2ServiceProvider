c6c7544a0b801f9b0fc2b770eff2af5a9e11dae7
fmartelli <fabio.martelli@gmail.com>
2015-01-13 11:45:51 +0100

    fix license and dependency version

--- src/main/java/net/tirasa/saml/store/SAMLSessionManager.java
+++ src/main/java/net/tirasa/saml/store/SAMLSessionManager.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2014 Tirasa.
+ * Copyright 2015 Tirasa.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/main/java/net/tirasa/saml/util/SAMLUtils.java
+++ src/main/java/net/tirasa/saml/util/SAMLUtils.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2014 Tirasa.
+ * Copyright 2015 Tirasa.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package net.tirasa.saml.util;
 
 import java.io.StringWriter;

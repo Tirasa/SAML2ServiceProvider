5f6268981b2ebe3e37ecb14b53d5f720e79fdaee
fmartelli <fabio.martelli@gmail.com>
2015-02-20 12:50:35 +0100

    extended metadata for slo

--- src/main/java/net/tirasa/saml/Consumer.java
+++ src/main/java/net/tirasa/saml/Consumer.java
@@ -13,21 +13,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package net.tirasa.saml;
 
+import static net.tirasa.saml.store.SAMLSessionManager.SAML_SESSION_INFO;
+
 import java.io.IOException;
+import java.rmi.ServerException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import net.tirasa.saml.context.COT;
+import net.tirasa.saml.context.IdP;
+import net.tirasa.saml.store.SAMLSessionInfo;
 import net.tirasa.saml.store.SAMLSessionManager;
 import net.tirasa.saml.util.Constants;
 import net.tirasa.saml.util.Properties;
 import net.tirasa.saml.util.SAMLUtils;
 import org.apache.commons.lang.StringUtils;
+import org.opensaml.common.SAMLException;
 import org.opensaml.common.SAMLObject;
 import org.opensaml.common.binding.SAMLMessageContext;
+import org.opensaml.saml2.core.LogoutRequest;
 import org.opensaml.saml2.core.NameID;
 import org.opensaml.saml2.core.Response;
 import org.slf4j.Logger;
@@ -44,23 +52,78 @@ public class Consumer extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(Consumer.class);
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * Processes AuthNResponses and LogoutRequests only.
      *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    protected void processRequest(final HttpServletRequest request, final HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
 
-        if (request.getRequestURL().toString().equals(Properties.getString(Constants.LOGOUT))) {
-            log.debug("Logout action: destroying SAML session.");
-            SAMLSessionManager.getInstance().destroySAMLSession(request.getSession());
-            // could be redirected to the idp if specified into IdP metadata (not yet supported)
-            return;
+        final String responseMessage = request.getParameter(Constants.SAML_AUTHN_RESPONSE_PARAMETER_NAME);
+        final String requestMessage = request.getParameter(Constants.SAML_AUTHN_REQUEST_PARAMETER_NAME);
+
+        if (StringUtils.isNotBlank(responseMessage)) {
+            log.debug("Check for SSO response: {}", responseMessage);
+            ssoResonse(request, response);
+        } else if (StringUtils.isNotBlank(requestMessage)) {
+            log.debug("Check for SLO request: {}", requestMessage);
+            sloRequest(request, response);
+        } else {
+            log.warn("Received empty message");
+            throw new ServerException("Received message is invalid");
         }
+    }
+
+    private void sloRequest(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
+
+        log.debug("Logout attempt: {}", request.getRequestURL().toString());
+
+        try {
+            final SAMLMessageContext<Response, SAMLObject, NameID> samlMessageContext = getSAMLMessageContext(
+                    request, response);
+
+            samlMessageContext.setLocalEntityId(
+                    Properties.getString(Constants.ENTITYID, request.getRequestURL().toString()));
+
+            final LogoutRequest inbound = LogoutRequest.class.cast(samlMessageContext.getInboundMessage());
+            final IdP idp = COT.getInstance().getIdP(samlMessageContext.getInboundMessageIssuer());
+
+            final HttpSession session = request.getSession();
+            if (SAMLSessionManager.getInstance().isSAMLSessionValid(session)) {
+
+                final Object info = session.getAttribute(SAML_SESSION_INFO);
+
+                if (info == null) {
+                    throw new Exception("Session info not found");
+                }
+
+                final SAMLSessionInfo sessionInfo = SAMLSessionInfo.class.cast(info);
+                log.debug("Session info: " + sessionInfo);
+
+                log.debug("Logout action: destroying SAML session.");
+                SAMLSessionManager.getInstance().destroySAMLSession(session);
+
+                log.info("Send SLO response to {}", idp.getId());
+                new SAMLResponseSender().sendLogoutResponse(
+                        request,
+                        response,
+                        idp,
+                        inbound.getID());
+            }
+
+        } catch (Exception e) {
+            log.warn("Received an invalid SAML2.0 message", e);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void ssoResonse(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
 
         log.debug("Attempt to secure resource  is intercepted : {}", request.getRequestURL().toString());
 
@@ -68,36 +131,45 @@ public class Consumer extends HttpServlet {
          * Check if response message is received from identity provider;
          * In case of successful response system redirects user to relayState (initial) request
          */
-        final String responseMessage = request.getParameter(Constants.SAML_AUTHN_RESPONSE_PARAMETER_NAME);
+        try {
 
-        if (StringUtils.isNotBlank(responseMessage)) {
-            log.debug("Response from Identity Provider is received");
-            try {
-                log.debug("Decoding of SAML message");
-                final SAMLMessageContext<Response, SAMLObject, NameID> samlMessageContext = SAMLUtils.decodeSamlMessage(
-                        request, response);
+            final SAMLMessageContext<Response, SAMLObject, NameID> samlMessageContext = getSAMLMessageContext(request,
+                    response);
 
-                log.debug("SAML message has been decoded successfully");
+            samlMessageContext.setLocalEntityId(
+                    Properties.getString(Constants.ENTITYID, request.getRequestURL().toString()));
 
-                samlMessageContext.setLocalEntityId(
-                        Properties.getString(Constants.ENTITYID, request.getRequestURL().toString()));
+            final String relayState = samlMessageContext.getRelayState();
 
-                final String relayState = samlMessageContext.getRelayState();
+            new SAMLResponseVerifier().verify(samlMessageContext);
 
-                new SAMLResponseVerifier().verify(samlMessageContext);
+            log.debug("Starting and store SAML session..");
+            SAMLSessionManager.getInstance().createSAMLSession(request.getSession(), samlMessageContext);
 
-                log.debug("Starting and store SAML session..");
-                SAMLSessionManager.getInstance().createSAMLSession(request.getSession(), samlMessageContext);
+            log.debug("User has been successfully authenticated in idP. Redirect to initial requested resource {}",
+                    relayState);
 
-                log.debug("User has been successfully authenticated in idP. "
-                        + "Redirect to initial requested resource {}", relayState);
-
-                response.sendRedirect(relayState);
-            } catch (Exception e) {
-                log.error("Error processing IdP response", e);
-                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            }
+            response.sendRedirect(relayState);
+        } catch (SAMLException | IOException e) {
+            log.error("Error processing IdP response", e);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
+
+    }
+
+    private SAMLMessageContext<Response, SAMLObject, NameID> getSAMLMessageContext(
+            final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException {
+        try {
+            log.debug("Decoding of SAML message");
+            final SAMLMessageContext<Response, SAMLObject, NameID> msg = SAMLUtils.decodeSamlMessage(request, response);
+            log.debug("Received SAML message:\n{}", SAMLUtils.SAMLObjectToString(msg.getInboundMessage()));
+            return msg;
+        } catch (Exception e) {
+            log.warn("Received an invalid SAML2.0 message");
+            throw new ServletException("Received message is invalid", e);
+        }
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
